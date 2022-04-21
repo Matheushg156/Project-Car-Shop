b7cfd1a7b9865b31f43f89fd55094738ec2130f6
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-04-20 22:58:10 -0300

    feat: create interface Car

---
+++ src/interfaces/CarInterface.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+import { VehicleSchema } from './VehicleInterface';
+
+export const CarSchema = VehicleSchema.extend({
+  doorsQty: z
+    .number({
+      required_error: 'Doors quantity is required',
+      invalid_type_error: 'Doors quantity must be a number',
+    })
+    .int()
+    .gte(2, { message: 'Doors quantity must be greater or equal than 2' })
+    .lte(4, { message: 'Doors quantity must be less or equal than 4' }),
+  seatsQty: z
+    .number({
+      required_error: 'Seats quantity is required',
+      invalid_type_error: 'Seats quantity must be a number',
+    })
+    .int()
+    .gte(2, { message: 'Seats quantity must be greater or equal than 2' })
+    .lte(7, { message: 'Seats quantity must be less or equal than 7' }),
+});
+
+export type Car = z.infer<typeof CarSchema>;
