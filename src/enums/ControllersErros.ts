4f793f0483527a564829315a5e3bbcc51d5936b8
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-04-24 17:27:32 -0300

    feat: create enum ControllersErros

---
+++ src/enums/ControllersErros.ts
@@ -0,0 +1,8 @@
+enum ControllersErros {
+  internal = 'Internal Server Error',
+  notFound = 'Object not found',
+  requiredId = 'Id is required',
+  badRequest = 'Bad request',
+}
+
+export default ControllersErros;
