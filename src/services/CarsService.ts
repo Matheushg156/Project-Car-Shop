5b7c1680fea2443ec582629161abdc3f9148fa25
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-04-23 23:50:08 -0300

    feat: create CarService

---
+++ src/services/CarsService.ts
@@ -0,0 +1,43 @@
+import { Car, CarSchema } from '../interfaces/CarInterface';
+import ServiceError from '../interfaces/ServiceError';
+import MongoService from './MongoService';
+import CarsModel from '../models/CarsModel';
+
+export default class CarsService extends MongoService<Car> {
+  private idLength = 24;
+
+  constructor(model = new CarsModel()) {
+    super(model);
+  }
+
+  public async create(data: Car): Promise<Car | ServiceError> {
+    const parsed = CarSchema.safeParse(data);
+    if (!parsed.success) {
+      return { error: parsed.error };
+    }
+    return this.model.create(data);
+  }
+
+  public async read(): Promise<Car[]> {
+    return this.model.read();
+  }
+
+  public async readOne(id: string): Promise<Car | null | ServiceError> {
+    return this.model.readOne(id);
+  }
+
+  public async update(id: string, data: Car):
+  Promise<Car | null | ServiceError> {
+    if (id.length !== this.idLength) return null;
+    const parsed = CarSchema.safeParse(data);
+    if (!parsed.success) {
+      return { error: parsed.error };
+    }
+    return this.model.update(id, data);
+  }
+
+  public async delete(id: string): Promise<Car | null | ServiceError> {
+    if (id.length !== this.idLength) return null;
+    return this.model.delete(id);
+  }
+}
