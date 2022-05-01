5e09216928e5b71392cf97898da592b722f99461
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-05-01 16:09:05 -0300

    feat: server

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,23 @@ import CustomRouter from './Routes/Router';
 import App from './app';
 
 import CarsController from './controllers/CarController';
+import MotorcyclesController from './controllers/MongoController';
 
 import { Car } from './interfaces/CarInterface';
+import { Motorcycle } from './interfaces/MotorcycleInterface';
 
 const server = new App();
 
 const carsController = new CarsController();
+const motorcyclesController = new MotorcyclesController();
 
 const CarRouter = new CustomRouter<Car>();
+const MotorcycleRouter = new CustomRouter<Motorcycle>();
 
 CarRouter.addRoute(carsController);
+MotorcycleRouter.addRoute(motorcyclesController);
 
 server.addRouter(CarRouter.router);
+server.addRouter(MotorcycleRouter.router);
 
 export default server;
